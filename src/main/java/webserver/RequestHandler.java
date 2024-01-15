1384a468cd9c866b7ced340f3ba860c8f8f17f2c
timido7021 <timido7021@gmail.com>
2024-01-15 13:38:10 +0900

    step-1: static html file response

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,20 +22,65 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+            String requestHeaderLine = "";
+            String method = "";
+            String requestUrl = "";
+
+            while (!(requestHeaderLine = bufferedReader.readLine().trim()).isEmpty()) {
+                logger.debug(requestHeaderLine);
+
+                String[] tokens = requestHeaderLine.split(" ");
+                if (tokens[0].equals("GET")
+                        || tokens[0].equals("POST")) {
+                    method = tokens[0];
+                    requestUrl = tokens[1];
+                }
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            byte[] body;
+            String contentType;
+            if (method.equals("GET")) {
+                if (requestUrl.equals("/index.html")) {
+                    body = Files.readAllBytes(
+                            new File("./src/main/resources/templates" + requestUrl).toPath());
+                    contentType = "text/html";
+                }
+                else  {
+                    body = Files.readAllBytes(
+                            new File("./src/main/resources/static" + requestUrl).toPath());
+
+                    if (requestUrl.endsWith(".js"))
+                        contentType = "text/javascript";
+                    else if (requestUrl.endsWith(".css"))
+                        contentType = "text/css";
+                    else if (requestUrl.endsWith(".ico"))
+                        contentType = "image/vnd.microsoft.icon";
+                    else if (requestUrl.endsWith(".ttf"))
+                        contentType = "text/ttf";
+                    else
+                        contentType = "text/plain";
+                }
+            } else {
+                body = "Hello World".getBytes();
+                contentType = "text/html";
+            }
+
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(String.format("Content-Type: %s;charset=utf-8\r\n", contentType));
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
