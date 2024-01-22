df7ae0055de3f661c47bdf3182b707bfb5c5c24a
timido7021 <timido7021@gmail.com>
2024-01-22 18:25:24 +0900

    test: createFromReader 테스트 코드 수정
    HttpRequest 객체를 만드는 메소드에 대한 테스트를 보강해서 queryString, Header, Body에 대한 assert문을 추가하였다.

--- src/test/java/http/HttpRequestTest.java
+++ src/test/java/http/HttpRequestTest.java
@@ -15,19 +15,28 @@ class HttpRequestTest {
     @Test
     @DisplayName("BufferedReader를 받아 HttpRequest 반환")
     void testCreateFromReader() throws IOException {
-        String requestString = "GET /index.html HTTP/1.1\r\n" +
+        String requestString = "GET /index.html?name=hong&age=26 HTTP/1.1\r\n" +
                 "Accept: text/html,*/*\r\n" +
                 "Accept-Encoding: gzip, deflate, br\r\n" +
                 "Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7\r\n" +
                 "Connection: keep-alive\r\n" +
                 "Host: localhost:8080\r\n" +
                 "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\r\n\r\n";
+
         BufferedReader bufferedReader = new BufferedReader(new StringReader(requestString));
 
         HttpRequest httpRequest = HttpRequest.createFromReader(bufferedReader);
 
         assertThat(httpRequest.getMethod()).isEqualTo("GET");
         assertThat(httpRequest.getUrl()).isEqualTo("/index.html");
-        assertThat(httpRequest.getQueryString()).isEmpty();
+        assertThat(httpRequest.getQueryString())
+                .containsEntry("name", "hong")
+                .containsEntry("age", "26");
+        assertThat(httpRequest.getRequestHeader().getProperties())
+                .containsEntry("Host", "localhost:8080")
+                .containsEntry("Accept", "text/html,*/*")
+                .containsEntry("Connection", "keep-alive");
+        assertThat(httpRequest.getRequestBody())
+                .hasFieldOrPropertyWithValue("rawString","");
     }
 }
