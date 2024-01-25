64a011d5416d35dbb4e67e606d55a12dee5891e5
timido7021 <timido7021@gmail.com>
2024-01-25 18:03:11 +0900

    fix: Session에서 sessionKey 필드 삭제
    해당 필드가 쓰이지도 않고 이미 sessionMap에서 sessionKey를 가지고 있으므로 의미가 중복된 필드라고 생각하여 제거함.

--- src/main/java/webserver/http/SessionManager.java
+++ src/main/java/webserver/http/SessionManager.java
@@ -13,13 +13,10 @@ public class SessionManager {
 
     private static class Session {
         private LocalDateTime createdAt;
-        private String sessionKey;
-
         private User user;
 
-        public Session(LocalDateTime createdAt, String sessionKey, User user) {
+        public Session(LocalDateTime createdAt, User user) {
             this.createdAt = createdAt;
-            this.sessionKey = sessionKey;
             this.user = user;
         }
 
@@ -27,20 +24,8 @@ public class SessionManager {
             return createdAt;
         }
 
-        public String getSessionKey() {
-            return sessionKey;
-        }
-
         public User getUser() {
             return user;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof String) {
-                return sessionKey.equals(obj);
-            }
-            return super.equals(obj);
         }
     }
 
@@ -49,12 +34,12 @@ public class SessionManager {
     public static String createSession(User user) {
         String randomKey = UUID.randomUUID().toString();
         LocalDateTime createdAt = LocalDateTime.now();
-        sessionMap.put(randomKey, new Session(createdAt, randomKey, user));
+        sessionMap.put(randomKey, new Session(createdAt, user));
 
-
+        // 현재 시간이랑 비교하여 5분을 초과한 만료된 세션 제거
         List<String> expired = sessionMap.entrySet().stream()
-                .filter(e -> e.getValue().getCreatedAt().plusMinutes(5).isBefore(createdAt))
-                .map(e -> e.getKey()).collect(Collectors.toList());
+                .filter(entry -> entry.getValue().getCreatedAt().plusMinutes(5).isBefore(createdAt))
+                .map(entry -> entry.getKey()).collect(Collectors.toList());
 
         expired.forEach(
                 key -> sessionMap.remove(key)
