6fb711bf99eae1d1adbb42f0600426316ee83b07
timido7021 <timido7021@gmail.com>
2024-01-29 10:33:46 +0900

    fix: 스레드풀 개수 변경 및 execute에서 submit 메소드로 변경
    execute는 비동기 테스크의 결과를 제어할 수 없지만, submit은 Future 클래스를 활용해서 비동기 테스크의 결과 및 동작을 제어할 수 있다. 그리고 스레드풀의 스레드 개수를 IO-Bound 작업이 많은 점에서 32개로 설정했다.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -12,7 +13,7 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) {
         int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
@@ -20,17 +21,22 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        // 스레드풀을 생성한다. 개수는 사용가능한 코어갯수에 비례하고,
+        // 정적 파일을 읽는 IO-Bound 작업이 많은 점에서 32개로 설정했다.
+        ExecutorService threadPool = Executors.newFixedThreadPool(32);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
-            ExecutorService threadPool = Executors.newFixedThreadPool(4);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                threadPool.execute(new RequestHandler(connection));
+                threadPool.submit(new RequestHandler(connection));
             }
-
+        } catch (IOException e) {
+            logger.debug("connection: " + e.getMessage());
+        } finally {
             threadPool.shutdown();
         }
     }
